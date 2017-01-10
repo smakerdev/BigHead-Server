d0e34a262d5759954052c964c1ea016fa27113b3
Smaker <admin@smaker.kr>
2017-01-10 19:27:02 +0900

    including DB

--- src/main/java/kr/smaker/bighead/controller/HomeController.java
+++ src/main/java/kr/smaker/bighead/controller/HomeController.java
@@ -1,12 +1,9 @@
-package kr.smaker.bighead;
+package kr.smaker.bighead.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
