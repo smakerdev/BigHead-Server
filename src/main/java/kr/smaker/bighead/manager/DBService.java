9a5a443c59a2986cf11855bd3395ef46422f8321
Smaker <admin@smaker.kr>
2017-01-11 21:42:38 +0900

    Troubleshooting DBService error

--- src/main/java/kr/smaker/bighead/manager/DBService.java
+++ src/main/java/kr/smaker/bighead/manager/DBService.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DBService {
 	
 	@Autowired
